78472ef1a5d06b0d8f6981e2c243e380fe984abc
supto-das <suptodas00@gmail.com>
2020-09-03 23:07:17 +0600

    Add Material Components

--- app/src/main/java/com/suptodas/diu/materialdesigncomponents/BottomNavigationActivity.java
+++ app/src/main/java/com/suptodas/diu/materialdesigncomponents/BottomNavigationActivity.java
@@ -1,9 +1,14 @@
 package com.suptodas.diu.materialdesigncomponents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -13,6 +18,7 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     BottomNavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,40 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
         badgeDrawable.setNumber(2);
         badgeDrawable.setVisible(true);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment;
+
+                if(item.getItemId() == R.id.favourite){
+                    fragment = new FavouriteFragment();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentLayout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                if(item.getItemId() == R.id.stared){
+                    fragment = new StaredFragment();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentLayout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                if(item.getItemId() == R.id.notificationNav){
+                    fragment = new NotificationFragment();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentLayout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                return true;
+            }
+        });
 
     }
 }
